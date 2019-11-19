67e1d2096be15fb4b5d99fbd4f9e1e4136d55e41
MARÍA ISABEL ORTIZ LIZCANO <MIsabel.Ortiz2@alu.uclm.es>
2019-11-19 08:54:47 +0100

    Creando conexion PDO para la bbdd

---
+++ conexionPDO.php
@@ -0,0 +1,13 @@
+<?php
+
+//iniciamos conexion
+
+try{
+    $conexionPDO = new PDO("mysql:host=localhost;dbname=mirinconfavorito; charset=utf8", "root", ""); 
+    $conexionPDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e){
+    die ('Error: '.$e->GetMessage());
+};
+
+?>
