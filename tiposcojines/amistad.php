4313857afe9879fbb2a374b411ed76540ad86c37
MARÍA ISABEL ORTIZ LIZCANO <MIsabel.Ortiz2@alu.uclm.es>
2019-11-21 09:47:53 +0100

    modelando añadir al carrito un producto

--- tiposcojines/amistad.php
+++ tiposcojines/amistad.php
@@ -32,7 +32,6 @@ function anadir_carro() {
     <div class="container">
         
         <br>
-        <form>
 
         <table>
 <!--1 fila-->
@@ -44,7 +43,7 @@ function anadir_carro() {
         <h3 class="text-center">Cojín amistad</h3>
         <br>
         <br>
-        <h5 class="text-center">12,00 - 14,00 €</h5>
+        <h5 class="text-center">13,00 €</h5>
         </th>
 
         </tr>
@@ -73,12 +72,16 @@ function anadir_carro() {
 <!--4 fila-->
         <tr>
         <td colspan="2">
-        <div class="form-group">
+        <form action="amistad.php" method="post">
                 <select name="genero_seleccionado" required="required" target="_blank">
                     <option value="mujer">Mujer</option>
                     <option value="hombre">Hombre</option>
                 </select>
-            </div><br>
+
+                <br><br>
+                <button type="submit"  value="anadir" onclick="anadir_carro()">Añadir al carrito</button>
+        <br><br>
+        </form>
         </td>
         <td>
         
@@ -87,13 +90,9 @@ function anadir_carro() {
         </table>
 
         
-            <br><br><br>
-            <div class="form-check">
-                <button class="btn btn-info" style="float: right; width:200px;" onclick="anadir_carro()">Añadir al carrito</button>
-            </div><br><br>
+           
             
 
-        </form>
 
     </div> <br><br>
 
@@ -110,20 +109,51 @@ function anadir_carro() {
 require '../conexionPDO.php';
 
 //vemos cuantos productos de este tipo hay para crear el id
-$sql = "SELECT count(*) FROM cojin_amistad ";
-$numeroproductos = $conexionPDO->query($sql);
-$numeroproductos=$numeroproducto->fetchColumn();
+$sql = "SELECT count(*) FROM cojin_amistad";
+//$numeroproductos = $conexionPDO->query($sql);
+//$numeroproductos=$numeroproducto->fetchColumn();
+
+$numeroproductos = 0;
+
+if ($res = $conexionPDO->query($sql)) {
+
+    /* Check the number of rows that match the SELECT statement */
+    if ($res->fetchColumn() > 0) {
+
+        /* Issue the real SELECT statement and work with the results */
+        $sql = "SELECT * FROM cojin_amistad";
+
+        foreach ($conexionPDO->query($sql) as $row) {
+            $numeroproductos++;
+        }
+    }
+    /* No rows matched -- do something else */ 
+}
+
+
 //creamos el id_producto
-$numero_id=(string)$numeroproductos+1;
+$numero_id=(string)($numeroproductos+1);
 $id_producto_creado = "pr".$numero_id;
 
 //recogemos la opcion seleccionada
-$genero = $_POST['genero_seleccionado'];
+if (isset($_POST['genero_seleccionado'])){
+    $genero = $_POST['genero_seleccionado'];
+}
+echo "id".$id_producto_creado;
+echo "genero".$genero;
+
+//$cojin_temporal = "INSERT INTO cojin_amistad (id_tipo_producto, id_producto, nombre_tipo, genero) VALUES ('2', 'pr1', 'Cojín Amistad', 'hombre')";
+//$conexionPDO->query($cojin_temporal);
 
 //añadimos (temporalmente, si el pedido no se realiza, se eliminará de la cookie y base de datos)
-$cojin_temporal = "INSERT INTO 'cojin_amistad' ('id_tipo_producto', 'id_producto', 'nombre_tipo', 'genero') VALUES ('2', '$id_producto_creado', 'Cojín Amistad', '$genero')";
-$cojin_temporal= "INSERT INTO `producto`(`id_producto`, `id_tipo_producto`, `precio_unidad`, `tamaño`) VALUES ('$id_producto_creado','2','12','40x40')";
-//$info = $conexionPDO->query($cojin_temporal);
+$cojin_temporal = "INSERT INTO cojin_amistad (id_tipo_producto, id_producto, nombre_tipo, genero) VALUES ('2', :id_producto_creado, 'Cojín Amistad', :genero)";
+$sentencia = $conexionPDO->prepare($cojin_temporal);
+$sentencia->execute(array(':id_producto_creado'=>$id_producto_creado, ':genero'=>$genero));
+
+$cojin_temporal= "INSERT INTO producto(id_producto, id_tipo_producto, precio_unidad, tamaño) VALUES (:id_producto_creado,'2','13','40x40')";
+$sentencia = $conexionPDO->prepare($cojin_temporal);
+$sentencia->execute(array(':id_producto_creado'=>$id_producto_creado));
+
 
 
 ?>
