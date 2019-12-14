b7cbfe84be789655cf7f65bf9135274ac16f5d76
CAROLINA ORDOÑO LOPEZ <Carolina.Ordono@alu.uclm.es>
2019-12-14 12:40:47 +0100

    Arreglada BBDD, insertados ids y campos faltantes. Arregladas dependencias

--- mirinconfavorito.sql
+++ mirinconfavorito.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-12-2019 a las 16:55:05
+-- Tiempo de generación: 14-12-2019 a las 12:34:30
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.10
 
@@ -50,17 +50,26 @@ INSERT INTO `cliente` (`id_cliente`, `nombre`, `apellidos`, `telefono`, `email`,
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cojines_corazon_dobles_senor-senora`
+-- Estructura de tabla para la tabla `cojines_corazon_dobles_senor_senora`
 --
 
-CREATE TABLE `cojines_corazon_dobles_senor-senora` (
+CREATE TABLE `cojines_corazon_dobles_senor_senora` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '1',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojines Corazón Doble Señor/Señora',
   `nombre_senor` varchar(20) NOT NULL,
   `nombre_senora` varchar(20) NOT NULL,
-  `fecha` date NOT NULL
+  `fecha` date NOT NULL,
+  `tipo_letra` enum('Mayúsculas','Minúsculas') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojines_corazon_dobles_senor_senora`
+--
+
+INSERT INTO `cojines_corazon_dobles_senor_senora` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `nombre_senor`, `nombre_senora`, `fecha`, `tipo_letra`) VALUES
+(1, '1', 'pr1', 'Cojines Corazón Doble Señor/Señora', 'Carlos', 'Cristina', '2019-12-19', 'Mayúsculas');
 
 -- --------------------------------------------------------
 
@@ -69,11 +78,19 @@ CREATE TABLE `cojines_corazon_dobles_senor-senora` (
 --
 
 CREATE TABLE `cojin_amistad` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '2',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Amistad',
   `genero` enum('hombre','mujer') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojin_amistad`
+--
+
+INSERT INTO `cojin_amistad` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `genero`) VALUES
+(1, '2', 'pr2', 'Cojín Amistad', 'mujer');
 
 -- --------------------------------------------------------
 
@@ -82,13 +99,22 @@ CREATE TABLE `cojin_amistad` (
 --
 
 CREATE TABLE `cojin_corazones_dobles` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '3',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojines Corazón Doble',
   `nombre_izquierda` varchar(20) NOT NULL,
   `nombre_derecha` varchar(20) NOT NULL,
-  `fecha` date NOT NULL
+  `fecha` date NOT NULL,
+  `tipo_letra` enum('Mayúsculas','Minúsculas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojin_corazones_dobles`
+--
+
+INSERT INTO `cojin_corazones_dobles` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `nombre_izquierda`, `nombre_derecha`, `fecha`, `tipo_letra`) VALUES
+(1, '3', 'pr3', 'Cojines Corazón Doble', 'Cristina', 'Carlos', '2019-12-19', 'Mayúsculas');
 
 -- --------------------------------------------------------
 
@@ -97,13 +123,22 @@ CREATE TABLE `cojin_corazones_dobles` (
 --
 
 CREATE TABLE `cojin_dibujo_individual` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '4',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Dibujo Individual',
   `nombre` varchar(20) NOT NULL,
   `dibujo` varchar(20) NOT NULL,
-  `fecha` date NOT NULL
+  `fecha` date NOT NULL,
+  `tipo_letra` enum('Mayúsculas','Minúsculas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojin_dibujo_individual`
+--
+
+INSERT INTO `cojin_dibujo_individual` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `nombre`, `dibujo`, `fecha`, `tipo_letra`) VALUES
+(1, '4', 'pr4', 'Cojín Dibujo Individual', 'Carol', 'Ordenador', '2019-12-04', 'Mayúsculas');
 
 -- --------------------------------------------------------
 
@@ -112,10 +147,19 @@ CREATE TABLE `cojin_dibujo_individual` (
 --
 
 CREATE TABLE `cojin_familia` (
+  `idinterno` varchar(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '5',
   `id_producto` varchar(8) NOT NULL,
-  `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Familia'
+  `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Familia',
+  `informacion_adicional` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojin_familia`
+--
+
+INSERT INTO `cojin_familia` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `informacion_adicional`) VALUES
+('1', '5', 'pr5', 'Cojín Familia', '');
 
 -- --------------------------------------------------------
 
@@ -124,6 +168,7 @@ CREATE TABLE `cojin_familia` (
 --
 
 CREATE TABLE `cojin_natalicio` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '6',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Natalicio',
@@ -136,18 +181,12 @@ CREATE TABLE `cojin_natalicio` (
   `color_secundario` enum('Gris','Azul','Azul Claro','Azul Cielo','Lila','Rosa','Rosa Claro','Coral','Rojo','Rojo Vino','Naranja','Amarillo','Verde','Verde Esmeralda') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `cojin_personalizado`
+-- Volcado de datos para la tabla `cojin_natalicio`
 --
 
-CREATE TABLE `cojin_personalizado` (
-  `id_tipo_producto` varchar(8) NOT NULL DEFAULT '7',
-  `id_producto` varchar(8) NOT NULL,
-  `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Personalizado',
-  `url_imagen` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `cojin_natalicio` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `nombre`, `hora`, `fecha`, `peso`, `medida`, `color_primario`, `color_secundario`) VALUES
+(1, '6', 'pr6', 'Cojín Natalicio', 'Carol', '04:00:00', '2019-07-01', 3, 53, 'Lila', 'Rosa Claro');
 
 -- --------------------------------------------------------
 
@@ -156,14 +195,24 @@ CREATE TABLE `cojin_personalizado` (
 --
 
 CREATE TABLE `cojin_profesion_doble` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '8',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojines Profesión Dobles',
   `profesion_izquierda` varchar(20) NOT NULL,
   `profesion_derecha` varchar(20) NOT NULL,
   `nombre_izquierda` varchar(20) NOT NULL,
-  `nombre_derecha` varchar(20) NOT NULL
+  `nombre_derecha` varchar(20) NOT NULL,
+  `fecha` date NOT NULL,
+  `tipo_letra` enum('Mayúsculas','Minúsculas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojin_profesion_doble`
+--
+
+INSERT INTO `cojin_profesion_doble` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `profesion_izquierda`, `profesion_derecha`, `nombre_izquierda`, `nombre_derecha`, `fecha`, `tipo_letra`) VALUES
+(1, '8', 'pr8', 'Cojines Profesión Dobles', 'Enfermera', 'Ingeniero', 'Cristina', 'Carlos', '2019-12-05', 'Mayúsculas');
 
 -- --------------------------------------------------------
 
@@ -172,16 +221,26 @@ CREATE TABLE `cojin_profesion_doble` (
 --
 
 CREATE TABLE `cojin_profesion_doble_sr_sra` (
-  `id_tipo_producto` varchar(8) NOT NULL DEFAULT '10',
+  `idinterno` int(11) NOT NULL,
+  `id_tipo_producto` varchar(8) NOT NULL DEFAULT '7',
   `id_producto` varchar(8) DEFAULT NULL,
-  `nombre` varchar(40) NOT NULL DEFAULT 'Cojines Profesión Dobles Sr Sra',
-  `srsraizquierda` varchar(5) NOT NULL,
+  `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojines Profesión Dobles Sr Sra',
+  `srsraizquierda` enum('Sr','Sra') NOT NULL,
   `profesionizquierda` varchar(20) NOT NULL,
   `nombreizquierda` varchar(20) NOT NULL,
-  `srsraderecha` varchar(5) NOT NULL,
+  `srsraderecha` enum('Sr','Sra') NOT NULL,
   `profesionderecha` varchar(20) NOT NULL,
-  `nombrederecha` varchar(20) NOT NULL
+  `nombrederecha` varchar(20) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `tipo_letra` enum('Mayúsculas','Minúsculas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cojin_profesion_doble_sr_sra`
+--
+
+INSERT INTO `cojin_profesion_doble_sr_sra` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `srsraizquierda`, `profesionizquierda`, `nombreizquierda`, `srsraderecha`, `profesionderecha`, `nombrederecha`, `fecha`, `tipo_letra`) VALUES
+(1, '7', 'pr7', 'Cojines Profesión Dobles Sr Sra', 'Sra', 'Enfermera', 'Cristina', 'Sr', 'Agricultor', 'Carlos', NULL, 'Mayúsculas');
 
 -- --------------------------------------------------------
 
@@ -190,20 +249,22 @@ CREATE TABLE `cojin_profesion_doble_sr_sra` (
 --
 
 CREATE TABLE `cojin_profesion_individual` (
+  `idinterno` int(11) NOT NULL,
   `id_tipo_producto` varchar(8) NOT NULL DEFAULT '9',
   `id_producto` varchar(8) NOT NULL,
   `nombre_tipo` varchar(40) NOT NULL DEFAULT 'Cojín Profesión Individual',
   `nombre` varchar(20) NOT NULL,
   `profesion` varchar(20) NOT NULL,
-  `fecha` date NOT NULL
+  `fecha` date NOT NULL,
+  `tipo_letra` enum('Mayúsculas','Minúsculas') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cojin_profesion_individual`
 --
 
-INSERT INTO `cojin_profesion_individual` (`id_tipo_producto`, `id_producto`, `nombre_tipo`, `nombre`, `profesion`, `fecha`) VALUES
-('9', 'pr1', 'Cojín Profesión Individual', 'Laura', 'Médica', '0000-00-00');
+INSERT INTO `cojin_profesion_individual` (`idinterno`, `id_tipo_producto`, `id_producto`, `nombre_tipo`, `nombre`, `profesion`, `fecha`, `tipo_letra`) VALUES
+(1, '9', 'pr1', 'Cojín Profesión Individual', 'Laura', 'Médica', '0000-00-00', 'Mayúsculas');
 
 -- --------------------------------------------------------
 
@@ -272,9 +333,19 @@ INSERT INTO `envio` (`id_envio`, `tipo_envio`, `costes_envio`) VALUES
 --
 
 CREATE TABLE `linea_miembro` (
-  `id_producto` varchar(8) NOT NULL,
+  `numero_linea_id` int(11) NOT NULL,
+  `idinterno_cojin` int(11) NOT NULL,
   `id_miembro` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `linea_miembro`
+--
+
+INSERT INTO `linea_miembro` (`numero_linea_id`, `idinterno_cojin`, `id_miembro`) VALUES
+(1, 1, '1'),
+(2, 1, '2'),
+(3, 1, '3');
 
 -- --------------------------------------------------------
 
@@ -283,6 +354,7 @@ CREATE TABLE `linea_miembro` (
 --
 
 CREATE TABLE `linea_producto` (
+  `num_linea` int(11) NOT NULL,
   `id_producto` varchar(8) NOT NULL,
   `id_pedido` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -291,8 +363,16 @@ CREATE TABLE `linea_producto` (
 -- Volcado de datos para la tabla `linea_producto`
 --
 
-INSERT INTO `linea_producto` (`id_producto`, `id_pedido`) VALUES
-('pr1', 1);
+INSERT INTO `linea_producto` (`num_linea`, `id_producto`, `id_pedido`) VALUES
+(1, 'pr1', 1),
+(2, 'pr2', 1),
+(3, 'pr3', 1),
+(4, 'pr4', 1),
+(5, 'pr5', 1),
+(6, 'pr6', 1),
+(7, 'pr7', 1),
+(8, 'pr8', 1),
+(9, 'pr9', 1);
 
 -- --------------------------------------------------------
 
@@ -306,6 +386,15 @@ CREATE TABLE `miembro` (
   `raza` varchar(8) NOT NULL,
   `tipo_familiar` enum('Padre','Madre','Hijo','Hija','Mascota') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `miembro`
+--
+
+INSERT INTO `miembro` (`id_miembro`, `nombre`, `raza`, `tipo_familiar`) VALUES
+('1', 'Carlos', 'Persona', 'Padre'),
+('2', 'Cristina', 'Persona', 'Madre'),
+('3', 'Carolina', 'Persona', 'Hija');
 
 -- --------------------------------------------------------
 
@@ -330,7 +419,8 @@ CREATE TABLE `pedido` (
 
 INSERT INTO `pedido` (`id_pedido`, `id_cliente`, `tipo_envio`, `cupon`, `precio_total`, `fecha_compra`, `anotaciones`, `estado`) VALUES
 (1, 'cl2', '1', NULL, 22, '2019-11-14', '', 'Tramitado'),
-(2, 'cl2', '1', NULL, 23, '2019-12-18', 'Ninguna', 'Enviado');
+(2, 'cl2', '1', NULL, 23, '2019-12-18', 'Ninguna', 'Enviado'),
+(3, 'cl2', 'Urgente', NULL, 11, '2019-12-11', '', 'Recibido');
 
 -- --------------------------------------------------------
 
@@ -350,7 +440,15 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`id_producto`, `id_tipo_producto`, `precio_unidad`, `tamaño`) VALUES
-('pr1', '9', 12, '30x50');
+('pr1', '1', 12, '30x50'),
+('pr2', '2', 22, '30x50'),
+('pr3', '3', 33, '40x40'),
+('pr4', '4', 44, '30x50'),
+('pr5', '5', 22, '30x50'),
+('pr6', '6', 22, '30x50'),
+('pr7', '7', 22, '30x50'),
+('pr8', '8', 22, '30x50'),
+('pr9', '9', 22, '30x50');
 
 --
 -- Índices para tablas volcadas
@@ -361,6 +459,60 @@ INSERT INTO `producto` (`id_producto`, `id_tipo_producto`, `precio_unidad`, `tam
 --
 ALTER TABLE `cliente`
   ADD UNIQUE KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `cojines_corazon_dobles_senor_senora`
+--
+ALTER TABLE `cojines_corazon_dobles_senor_senora`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_amistad`
+--
+ALTER TABLE `cojin_amistad`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_corazones_dobles`
+--
+ALTER TABLE `cojin_corazones_dobles`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_dibujo_individual`
+--
+ALTER TABLE `cojin_dibujo_individual`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_familia`
+--
+ALTER TABLE `cojin_familia`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_natalicio`
+--
+ALTER TABLE `cojin_natalicio`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_profesion_doble`
+--
+ALTER TABLE `cojin_profesion_doble`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_profesion_doble_sr_sra`
+--
+ALTER TABLE `cojin_profesion_doble_sr_sra`
+  ADD PRIMARY KEY (`idinterno`);
+
+--
+-- Indices de la tabla `cojin_profesion_individual`
+--
+ALTER TABLE `cojin_profesion_individual`
+  ADD PRIMARY KEY (`idinterno`);
 
 --
 -- Indices de la tabla `cupones`
@@ -384,13 +536,13 @@ ALTER TABLE `envio`
 -- Indices de la tabla `linea_miembro`
 --
 ALTER TABLE `linea_miembro`
-  ADD PRIMARY KEY (`id_producto`,`id_miembro`);
+  ADD PRIMARY KEY (`numero_linea_id`);
 
 --
 -- Indices de la tabla `linea_producto`
 --
 ALTER TABLE `linea_producto`
-  ADD PRIMARY KEY (`id_pedido`,`id_producto`) USING BTREE;
+  ADD PRIMARY KEY (`num_linea`);
 
 --
 -- Indices de la tabla `miembro`
@@ -415,10 +567,64 @@ ALTER TABLE `producto`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cojines_corazon_dobles_senor_senora`
+--
+ALTER TABLE `cojines_corazon_dobles_senor_senora`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_amistad`
+--
+ALTER TABLE `cojin_amistad`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_corazones_dobles`
+--
+ALTER TABLE `cojin_corazones_dobles`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_dibujo_individual`
+--
+ALTER TABLE `cojin_dibujo_individual`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_natalicio`
+--
+ALTER TABLE `cojin_natalicio`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_profesion_doble`
+--
+ALTER TABLE `cojin_profesion_doble`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_profesion_doble_sr_sra`
+--
+ALTER TABLE `cojin_profesion_doble_sr_sra`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cojin_profesion_individual`
+--
+ALTER TABLE `cojin_profesion_individual`
+  MODIFY `idinterno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `linea_producto`
+--
+ALTER TABLE `linea_producto`
+  MODIFY `num_linea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT de la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id_pedido` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pedido` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
