d3248269c1da5c11b0bd298b6255172aff46ebe9
MARÍA ISABEL ORTIZ LIZCANO <MIsabel.Ortiz2@alu.uclm.es>
2019-12-15 16:38:56 +0100

    añadiendo indices

--- tiposcojines/dibujosimple.php
+++ tiposcojines/dibujosimple.php
@@ -165,7 +165,10 @@ if (isset($_POST['nombre_apellido']) && isset($_POST['dibujo'])) {
         }
         /* No rows matched -- do something else */
     }
-
+  //vemos cuantos productos de este tipo hay en el carrito para crear el id
+  if (isset($_SESSION['carrito'])){
+    $numeroproductos+=count($_SESSION['carrito']);           
+}//if set carrito
 
 
     //creamos el id_producto
